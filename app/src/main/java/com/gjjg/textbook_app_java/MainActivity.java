71b05f6c7c7e03b1420d55e94d7532adc91189fc
Gabriel Fardoski <43648010+GabrielGPF@users.noreply.github.com>
2021-06-19 17:35:12 -0300

    Test buttons created

--- app/src/main/java/com/gjjg/textbook_app_java/MainActivity.java
+++ app/src/main/java/com/gjjg/textbook_app_java/MainActivity.java
@@ -3,6 +3,7 @@ package com.gjjg.textbook_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signinButtonClick(View view) {
+    }
+
+    public void signupButtonClick(View view) {
+    }
+
+    public void gerarAsciiArtButtonClick(View view) {
     }
 }
